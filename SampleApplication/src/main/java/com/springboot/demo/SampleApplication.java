9d67301532bc67585e490cb40af4448c8bce71eb
sankar <sankar3735@gmail.com>
2017-11-27 21:46:37 +0530

    added

---
+++ SampleApplication/src/main/java/com/springboot/demo/SampleApplication.java
@@ -0,0 +1,26 @@
+package com.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
+	
+	//https://github.com/Dotridge/springcloudrepo
+	
+
+	@RestController
+	class MyRestController{
+		@RequestMapping("/say")
+		public String sayHello(){
+			return "Hi This is spring boot application!";
+		}
+	}
+	
+}
